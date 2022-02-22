8403d7ffab2cc38dbfe5aec8ec69da83a3f946d2
stphn <cretestephen@gmail.com>
2022-02-22 14:19:19 +0100

    Gif file added

--- src/views/detailBookCardCreate.js
+++ src/views/detailBookCardCreate.js
@@ -8,23 +8,23 @@ export const DetailBookCardCreate = (book) => {
 	imageElement.alt = book?.volumeInfo?.title;
 	const authorElement = createElement("h3", "author");
 	authorElement.innerText = book?.volumeInfo?.authors[0];
-	const otherBookElement = createElement("h5");
-	otherBookElement.innerText = "See other books of the author.";
 	firstWrapperElement.appendChild(imageElement);
 	firstWrapperElement.appendChild(authorElement);
-	firstWrapperElement.appendChild(otherBookElement);
 	detailBookElement.appendChild(firstWrapperElement);
 	const secondWrapperElement = createElement("div", "infoWrapper");
 	const bookTitleElement = createElement("h3");
 	const descriptionElement = createElement("p");
-	const detailInfoElement = createElement("p", "smallCase");
+	const detailInfoElement = createElement("h6", "smallCase");
+	const detailSecondInfoElement = createElement("h6", "smallCase");
 	secondWrapperElement.appendChild(bookTitleElement);
 	bookTitleElement.innerText = book?.volumeInfo?.title;
 	secondWrapperElement.appendChild(descriptionElement);
 	descriptionElement.innerText = book?.volumeInfo?.description;
 	secondWrapperElement.appendChild(detailInfoElement);
-	detailInfoElement.innerText = `Category: ${book?.volumeInfo?.categories[0]}, ISBN: ${book?.volumeInfo?.industryIdentifiers[1]?.identifier} <br /> Publish Date: ${book?.volumeInfo?.publishedDate}, Page Count:${book?.volumeInfo?.pageCount}
+	detailInfoElement.innerText = `Category: ${book?.volumeInfo?.categories[0]}, ISBN: ${book?.volumeInfo?.industryIdentifiers[1]?.identifier}  
   `;
+	detailSecondInfoElement.innerText = `Publish Date: ${book?.volumeInfo?.publishedDate}, Page Count:${book?.volumeInfo?.pageCount}`;
+	secondWrapperElement.appendChild(detailSecondInfoElement);
 	detailBookElement.appendChild(secondWrapperElement);
 
 	return detailBookElement;
