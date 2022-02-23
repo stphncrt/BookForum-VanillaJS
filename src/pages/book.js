942de5dfd72a3073e4ace89d453788fe8aa3c22f
stphn <cretestephen@gmail.com>
2022-02-23 17:31:34 +0100

    Revised App

--- src/pages/book.js
+++ src/pages/book.js
@@ -1,14 +1,10 @@
 "use strict";
 import createElement from "../helper/createElement.js";
 import { BooksContainerCreate } from "../views/booksContainer.js";
-import { navbarCreate } from "../views/navBar.js";
 
-export const bookPage = (userInterface, searchText) => {
-	userInterface.innerHTML = "";
+export const bookPage = (searchText) => {
 	const contentElement = createElement("div", "book-page");
-	const navBarElement = navbarCreate();
 	const booksContainerElement = BooksContainerCreate(searchText);
-	contentElement.appendChild(navBarElement);
 	contentElement.appendChild(booksContainerElement);
-	userInterface.appendChild(contentElement);
+	return contentElement;
 };
--- src/pages/startPage.js
+++ src/pages/startPage.js
@@ -2,18 +2,24 @@
 import createElement from "../helper/createElement.js";
 import { navbarCreate } from "../views/navBar.js";
 import { challengeImgCreate } from "../views/challengeImg.js";
-import { createContentText } from "../views/contentText.js";
 import { createInputBox } from "../views/inputBox.js";
+import { BooksContainerCreate } from "../views/booksContainer.js";
+import { createContentText } from "../views/contentText.js";
 
 export const initStartPage = (userInterface, searchText) => {
+	if (userInterface) {
+		userInterface.innerHTML = "";
+	}
 	const contentElement = createElement("div", "startPage-content-container");
 	const navBarElement = navbarCreate();
-	const challengeContainerElement = challengeImgCreate();
-	const inputElement = createInputBox(searchText);
 	const contentTextElement = createContentText();
+	const booksContent = BooksContainerCreate(searchText);
+	const challengeContainerElement = challengeImgCreate();
+	const inputElement = createInputBox();
 	contentElement.appendChild(navBarElement);
 	contentElement.appendChild(challengeContainerElement);
 	contentElement.appendChild(inputElement);
 	contentElement.appendChild(contentTextElement);
+	contentElement.appendChild(booksContent);
 	userInterface.appendChild(contentElement);
 };
